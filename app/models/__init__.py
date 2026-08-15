c2161f9572163585fcb46f194dce6fed54fee9c3
harshitkaur-0311 <harshitkaur0311@gmail.com>
2026-08-15 19:36:15 +0530

    push app/api/v1

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -9,4 +9,4 @@ from app.models.train_location import TrainLocation
 from app.models.crowd_log import CrowdLog
 from app.models.prediction import Prediction
 from app.models.alert import Alert
-from app.models.ai_model import AIModel
+from app.models.ai_model import AIModelX
